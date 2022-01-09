dc7a403f876d6cd99652576a08202f02cb27e3a1
marq <makolodziej86@gmail.com>
2022-01-09 21:08:15 +0100

    nav ended

--- js/main.js
+++ js/main.js
@@ -10,7 +10,19 @@ const handleNav = () => {
       item.addEventListener('click', () =>{
         nav.classList.remove('nav--active') 
       })  
-    });
+    })
+
+    handleNavItemsAnimation();
+}
+
+const handleNavItemsAnimation = () => {
+    let delayTime = 0;
+
+    allNavItems.forEach(item => {
+      item.classList.toggle('nav-items-animation')  
+      item.style.animationDelay = '.' + delayTime + 's';
+      delayTime++;
+    })
 }
 
 navBtn.addEventListener('click', handleNav)
